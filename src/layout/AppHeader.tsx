fa90d85572f2ace86d126242d08596aaed767a69
amirKeb2 <amirkebede4@gmail.com>
2025-03-25 06:27:13 +0300

    auth pages

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -84,17 +84,17 @@ return (
 
         <Link href="/" className="lg:hidden">
             <Image
-            width={154}
+            width={50}
             height={32}
             className="dark:hidden"
             src="./images/logo/logo.svg"
             alt="Logo"
             />
             <Image
-            width={154}
+            width={50}
             height={32}
             className="hidden dark:block"
-            src="./images/logo/logo-dark.svg"
+            src="./images/logo/logo.svg"
             alt="Logo"
             />
         </Link>
